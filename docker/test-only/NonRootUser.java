d67e22c51557595a95f62f6745ae4c6b82de38f8
Bruno Borges <bruno.borges@gmail.com>
2022-12-05 02:03:45 -0300

    fix formatting

--- docker/test-only/NonRootUser.java
+++ docker/test-only/NonRootUser.java
@@ -1,10 +1,10 @@
 public class NonRootUser {
- public static void main(String[] args) {
-  String expectedUser = "app";
-  String user = System.getProperty("user.name");
-  assert user != null : "user.name is null";
-  assert expectedUser != null : "expectedUser is null";
-  assert expectedUser.equals(user) : "user.name is not '" + expectedUser + "'";
-  System.out.println("SUCCESS: User is " + user);
- }
+    public static void main(String[] args) {
+        String expectedUser = "app";
+        String user = System.getProperty("user.name");
+        assert user != null : "user.name is null";
+        assert expectedUser != null : "expectedUser is null";
+        assert expectedUser.equals(user) : "user.name is not '" + expectedUser + "'";
+        System.out.println("SUCCESS: User is " + user);
+    }
 }

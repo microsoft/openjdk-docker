c54e7e617428bef0054c8e96a7e573e75923b848
joe-braley <earlejoebraley@gmail.com>
2025-12-15 14:42:58 -0800

    Wip: remove image from script

--- scripts/image-annotation.sh
+++ scripts/image-annotation.sh
@@ -8,31 +8,14 @@ fi
 
 debug=false
 
-while getopts "r:i:m:d" opt; do
+while getopts "r:m:d" opt; do
     case $opt in
         r) registry="$OPTARG" ;;
-        i) image="$OPTARG" ;;
         m) manifest="$OPTARG" ;;
         d) debug=true ;;
         *) echo "Invalid option: -$OPTARG" ;;
     esac
 done
-
-# IFS=';' read -ra REGISTRIES_ARRAY <<< "$REGISTRIES"
-
-# for REGISTRY in "${REGISTRIES_ARRAY[@]}"; do
-# echo "Pulling... $REGISTRY"
-# echo "Pulling... $registry/$image@$digest"
-
-# docker pull "$registry/$image@$digest"
-# if [[ $? -ne 0 ]]; then
-#     echo "Failed to pull image $registry/$tag"
-#     exit 1
-# fi
-
-# manifest=$(docker image inspect "$registry/$tag" | jq)
-# digest=$(echo $manifest | jq '.[0].RepoDigests[0]')
-# digest=${digest//\"/}
 
 if [[ -z "$manifest" ]]; then
     echo "##vso[task.logissue type=error]Container image manifest is empty or null. Unable to add annotation!"
@@ -40,17 +23,17 @@ fi
 
 endOfLifeDate=$(date "+%Y-%m-%d")
 
-echo "Annotating image $registry/$image@$manifest with end-of-life date $endOfLifeDate"
+echo "Annotating image $registry@$manifest with end-of-life date $endOfLifeDate"
 
 if [[ "$debug" == "true" ]]; then
     echo "[DRY-RUN] Running in dry-run mode. No changes will be made."
     echo "[DRY-RUN] Command that would be executed:"
-    echo "oras attach --artifact-type \"application/vnd.microsoft.artifact.lifecycle\" --annotation \"vnd.microsoft.artifact.lifecycle.end-of-life.date=${endOfLifeDate}T00:00:00Z\" $registry/$image@$manifest --verbose"
+    echo "oras attach --artifact-type \"application/vnd.microsoft.artifact.lifecycle\" --annotation \"vnd.microsoft.artifact.lifecycle.end-of-life.date=${endOfLifeDate}T00:00:00Z\" $registry@$manifest --verbose"
 else
     oras attach \
     --artifact-type "application/vnd.microsoft.artifact.lifecycle" \
     --annotation "vnd.microsoft.artifact.lifecycle.end-of-life.date=${endOfLifeDate}T00:00:00Z" \
-    $registry/$image@$manifest --verbose
+    $registry@$manifest --verbose
 
     if [[ $? -ne 0 ]]; then
         echo "Failed to annotate image!"

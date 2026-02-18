9418d83356cdef367b1423a4cdf59a832043329a
Joe Braley <106336504+joe-braley@users.noreply.github.com>
2026-02-18 09:16:12 -0800

    Update the docker build process (#142)
    * wip: adjust how we call signing process
    
    * Update annotations
    
    * Add retries for build image
    
    * Add short digest tag
    
    * Add missing args
    
    * PR feedback
    
    * Add condition
    
    * PR feedback
    
    * Wip: Fix args
    
    ---------
    
    Co-authored-by: joe-braley <joebraley@microsoft.com>

--- scripts/image-annotation.sh
+++ scripts/image-annotation.sh
@@ -1,36 +1,53 @@
 #!/bin/bash
-az acr login -n msopenjdk
+az acr login -n "$ACR_NAME"
 
 if [[ $? -ne 0 ]]; then
     echo "Failed to login to ACR"
     exit 1
 fi
 
-IFS=';' read -ra REGISTRIES_ARRAY <<< "$REGISTRIES"
+debug=false
 
-for REGISTRY in "${REGISTRIES_ARRAY[@]}"; do
-    echo "Pulling... $REGISTRY"
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -r | --registry)
+            registry="$2"
+            shift 2
+            ;;
+        -m | --manifest)
+            manifest="$2"
+            shift 2
+            ;;
+        -d | --debug)
+            debug=true;
+            shift 1
+            ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+done
 
-    docker pull "$REGISTRY"
-    if [[ $? -ne 0 ]]; then
-        echo "Failed to pull image $REGISTRY"
-        exit 1
-    fi
+if [[ -z "$manifest" ]]; then
+    echo "##vso[task.logissue type=error]Container image manifest is empty or null. Unable to add annotation!"
+    exit 1
+fi
 
-    manifest=$(docker image inspect "$REGISTRY" | jq)
-    digest=$(echo $manifest | jq '.[0].RepoDigests[0]')
-    digest=${digest//\"/}
-    endOfLifeDate=$(date "+%Y-%m-%d")
+endOfLifeDate=$(date "+%Y-%m-%d")
 
-    echo "Annotating image $digest with end-of-life date $endOfLifeDate"
+echo "Annotating image ${registry}@${manifest} with end-of-life date ${endOfLifeDate}T00:00:00"
+
+if [[ "$debug" == true ]]; then
+    echo "[DRY-RUN] Running in dry-run mode. No changes will be made."
+    echo "[DRY-RUN] Command that would be executed:"
+    echo "oras attach --artifact-type \"application/vnd.microsoft.artifact.lifecycle\" --annotation \"vnd.microsoft.artifact.lifecycle.end-of-life.date=${endOfLifeDate}T00:00:00Z\" $registry@$manifest --verbose"
+else
     oras attach \
     --artifact-type "application/vnd.microsoft.artifact.lifecycle" \
     --annotation "vnd.microsoft.artifact.lifecycle.end-of-life.date=${endOfLifeDate}T00:00:00Z" \
-    $digest --verbose
+    $registry@$manifest \
+    --verbose
 
     if [[ $? -ne 0 ]]; then
         echo "Failed to annotate image!"
         exit 1
     fi
-
-done
+fi
